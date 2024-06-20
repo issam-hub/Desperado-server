a8942c1c21faea972b9a331959836e5faac92725
bachirovvv <issambachir@yahoo.com>
2024-06-20 16:46:32 +0200

    working on exploit the server even more

--- main.c
+++ main.c
@@ -1,18 +1,43 @@
 #include "server.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <time.h>
+
+
+void getHTMLindexFile(char* buffer){
+    FILE *fp;
+    if((fp = fopen("./index.html", "a+"))==NULL){
+        perror("can't open index.html file\n");
+        exit(1);
+    }else{
+        int c;
+        int i = 0;
+        while((c = getc(fp))!=EOF){
+            buffer[i++] = c;
+        }
+    }
+}
 
 void run(Server *Server){ 
     char buffer[30000];
     int addressLength = sizeof(Server->address);
-    char* response = "HTTP/1.1 200 OK\n"
-    "Date: Mon, 27 Jul 2009 12:28:53 GMT\n"
+    char* datetime_format = "%a, %d %b %Y %H:%M:%S GMT";
+    char datetime_buffer[100];
+    time_t t = time(NULL);
+    
+    strftime(datetime_buffer, sizeof(datetime_buffer), datetime_format, gmtime(&t));
+    char response[10000];
+    char html[2048];
+    getHTMLindexFile(html);
+    sprintf(response, "HTTP/1.1 200 OK\n"
+    "Date: %s\n"
     "Server: Desperado/1.0.0 (Win64)\n"
-    "Last-Modified: Wed, 22 Jul 2009 19:15:56 GMT\n"
+    // "Last-Modified: Wed, 22 Jul 2009 19:15:56 GMT\n"
     "Content-Type: text/html\n"
     "Connection: Closed\n\n"
-    "<html><body><h1>Hello, World!</h1></body></html>";
+    "%s", datetime_buffer, html);
     int newSocket;
     while(1){
         printf("====== WAITING ======\n");
